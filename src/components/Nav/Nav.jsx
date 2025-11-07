00c5ce224987695acc7b76bac814294da86375ca
vikzaicev <vikzaicev@mail.ru>
2025-11-07 12:24:38 +0500

    dark mode

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { NavLink } from "react-router";
 import sun from "./../../img/icons/sun.svg";
 import moon from "./../../img/icons/moon.svg";
 
 function Nav() {
+  const [dark, setDark] = useState(false);
+  const btnRef = useRef(null);
+
+  const BtnClickHandler = () => {
+    setDark((prev) => !prev);
+  };
+  console.log(btnRef.current);
+
+  useEffect(() => {
+    if (dark) {
+      document.body.classList.add("dark");
+      btnRef.current.classList.add("dark-mode-btn--active");
+    } else {
+      document.body.classList.remove("dark");
+      btnRef.current.classList.remove("dark-mode-btn--active");
+    }
+  }, [dark]);
+
   return (
     <nav className="nav">
       <div className="container">
@@ -15,7 +33,14 @@ function Nav() {
             <strong>Freelancer</strong> portfolio
           </a> */}
 
-          <button className="dark-mode-btn">
+          <button
+            ref={btnRef}
+            onClick={BtnClickHandler}
+            // className={
+            //   dark ? "dark-mode-btn" : "dark-mode-btn dark-mode-btn--active"
+            // }
+            className="dark-mode-btn"
+          >
             <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
             <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
           </button>
